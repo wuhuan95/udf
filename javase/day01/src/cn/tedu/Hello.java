85bc708f63242b717aeb88a0bba61a265c502cf2
wu1575669840 <15990055752@163.com>
2018-11-30 23:12:49 +0800

    java

---
+++ javase/day01/src/cn/tedu/Hello.java
@@ -0,0 +1,11 @@
+package cn.tedu;
+
+/**
+ * @author wuh
+ * @Created by wuh on 2018/6/21.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("I Love Java");
+    }
+}
